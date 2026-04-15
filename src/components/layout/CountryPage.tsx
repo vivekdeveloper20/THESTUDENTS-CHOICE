8249e0338cc44b7d0da3ee26fe2b46507b5b1750
vivekdeveloper20 <vk1124629@gmail.com>
2026-04-15 22:44:24 +0530

    second

--- src/components/layout/CountryPage.tsx
+++ src/components/layout/CountryPage.tsx
@@ -25,7 +25,7 @@ const fadeUp = {
   show: (i: number) => ({
     opacity: 1,
     y: 0,
-    transition: { delay: i * 0.1, duration: 0.6, ease: [0.22, 1, 0.36, 1] },
+    transition: { delay: i * 0.1, duration: 0.6, ease: [0.22, 1, 0.36, 1] as any },
   }),
 };
 
